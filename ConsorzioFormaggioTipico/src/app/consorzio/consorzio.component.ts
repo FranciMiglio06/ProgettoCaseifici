66cd8fd341558e6d659b7b2301a4be1eff6ff1d5
Faloigi <65950084+Faloigi@users.noreply.github.com>
2025-02-21 18:42:08 +0100

    p-galleria implementato
    Ho implementato p-galleria nella home con 2 immagini di test e con uno stile.

--- ConsorzioFormaggioTipico/src/app/consorzio/consorzio.component.ts
+++ ConsorzioFormaggioTipico/src/app/consorzio/consorzio.component.ts
@@ -4,14 +4,34 @@ import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HeaderComponent } from '../header/header.component';
+import { GalleriaModule} from 'primeng/galleria';
 
 @Component({
   selector: 'consorzio',
   standalone: true,
-  imports: [RouterOutlet, CommonModule,FormsModule,CaseificioComponent,HeaderComponent],
+  imports: [RouterOutlet, CommonModule,FormsModule,CaseificioComponent,HeaderComponent,GalleriaModule],
   templateUrl: './consorzio.component.html',
   styleUrl: './consorzio.component.scss'
 })
 export class ConsorzioComponent {
+  images = [
+    {
+      itemImageSrc: "/assets/images/logo.png",
+      thumbnailImageSrc: "/assets/images/logo.png",
+      alt: "Logo",
+      title: "Logo"
+    },
+    {
+      itemImageSrc: "/assets/images/0.jpg",
+      thumbnailImageSrc: "/assets/images/0.jpg",
+      alt: "Logo1",
+      title: "Logo1"
+    }
+  ];
 
+  responsiveOptions = [
+    { breakpoint: '1024px', numVisible: 3 },
+    { breakpoint: '768px', numVisible: 2 },
+    { breakpoint: '560px', numVisible: 1 }
+  ];
 }
