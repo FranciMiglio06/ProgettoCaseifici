61307e422ba7c5e00b952b53b8387ef17ec4e18c
FranciMiglio06 <migliorucci.francesco@franchettisalviani.net>
2025-02-21 13:25:29 +0100

    Conclusione header inizio footer

--- ConsorzioFormaggioTipico/src/app/header/header.component.ts
+++ ConsorzioFormaggioTipico/src/app/header/header.component.ts
@@ -1,9 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { ImageModule } from 'primeng/image';
+import { ButtonModule } from 'primeng/button';
 @Component({
   selector: 'header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,FormsModule,ImageModule,ButtonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
