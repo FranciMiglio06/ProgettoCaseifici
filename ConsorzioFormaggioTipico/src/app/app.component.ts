dcb90f8b870d96a987aa9d9ad6e2a8cd43601b17
FranciMiglio06 <migliorucci.francesco@franchettisalviani.net>
2025-02-28 13:24:02 +0100

    Inizio gestione servizio backend

--- ConsorzioFormaggioTipico/src/app/app.component.ts
+++ ConsorzioFormaggioTipico/src/app/app.component.ts
@@ -4,13 +4,23 @@ import { CaseificioComponent } from './caseificio/caseificio.component';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { ConsorzioComponent } from './consorzio/consorzio.component';
+import { BackendService } from './backend.service';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule,FormsModule,ConsorzioComponent],
+  imports: [RouterOutlet, CommonModule,FormsModule,ConsorzioComponent,BackendService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'ConsorzioFormaggioTipico';
+  data: any[] = [];
+
+  constructor(private apiService: BackendService) {}
+
+  ngOnInit() {
+    this.apiService.getData().subscribe(response => {
+      this.data = response;
+    });
+  }
 }
