eeb13f25ac41ae021acd3c49b2ca36efec1a78a3
G4bbino <disikoci@gmail.com>
2025-03-05 11:38:29 +0100

    Inizio metodoGetForma

--- ConsorzioFormaggioTipico/src/app/Config/metodi.php
+++ ConsorzioFormaggioTipico/src/app/Config/metodi.php
@@ -22,6 +22,34 @@ if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true) {
         public $indirizzo;
         public $cli_id;
     }
+
+    class Acquisti {
+        public  $acq_id;
+        public $acq_cli_id;
+        public $acq_for_id;
+        public $acq_tip_id;
+        public function __construct($cliente, $tipo_forma, $forme, $tipologie) {
+            $this->acq_cli_id = $cliente;
+            $this->acq_tip_id = $tipologie;
+            $this->acq_for_id = $forme; 
+    }
+}
+    class forme{
+        public $for_id;
+        public $for_data;
+        public $for_num_forma;
+        public $for_nome;
+        public $for_peso;
+        public $for_scelta;
+        public $for_stag_eff;
+        public $for_venduta;
+        public $for_data_acquisto;
+        public $for_dat_id;
+        public $for_prezzo_reale;
+        public $for_tip_id;
+        public $for_sta_id;
+    }
+    
     
         function caseifici($conn){
         
@@ -98,10 +126,11 @@ if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true) {
             }
             return null; // Se non c'è corrispondenza, restituisce null
         }
-    Function compra(){
-        
-    }
-    Function getForma(){
-                
+        Function compra($cliente,$tipo_forma,$forme,$tipologie){
+            $acquisto = new Acquisti($cliente, $tipo_forma, $forme, $tipologie);  
+            return $acquisto;
+        }    
+    Function getForma($forma){
+        return $forma;//da finire//
     }
 }
