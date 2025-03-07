eea08e43c51223c2ab9170308728ee267893bed4
FranciMiglio06 <migliorucci.francesco@franchettisalviani.net>
2025-03-07 13:34:54 +0100

    Conclusione header

--- ConsorzioFormaggioTipico/src/app/app.routes.ts
+++ ConsorzioFormaggioTipico/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { CaseificiComponent } from './caseifici/caseifici.component';
+import { ContattiComponent } from './contatti/contatti.component';
+import { CarrelloComponent } from './carrello/carrello.component';
+import { ConsorzioComponent } from './consorzio/consorzio.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'consorzio', pathMatch: 'full' }, 
+  { path: 'consorzio', component: ConsorzioComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'caseifici', component: CaseificiComponent },
+  { path: 'contatti', component: ContattiComponent },
+  { path: 'carrello', component: CarrelloComponent },
+  { path: '**', redirectTo: 'consorzio' } 
+];
--- ConsorzioFormaggioTipico/src/app/header/header.component.ts
+++ ConsorzioFormaggioTipico/src/app/header/header.component.ts
@@ -3,18 +3,24 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ImageModule } from 'primeng/image';
 import { ButtonModule } from 'primeng/button';
+import { Router } from '@angular/router'
 @Component({
   selector: 'header',
   standalone: true,
-  imports: [CommonModule,FormsModule,ImageModule,ButtonModule],
+  imports: [CommonModule,FormsModule,ImageModule,ButtonModule,Router],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 
 export class HeaderComponent {
+  constructor(private router: Router) {}
   onButtonHover(event: any) {
     // l'hover button
     event.target.style.backgroundColor = '#B8860B';
+  }
+  navigateTo(route: string) {
+    // Redirect alla pagina selezionata
+    this.router.navigate([route]); 
   }
 }
 
